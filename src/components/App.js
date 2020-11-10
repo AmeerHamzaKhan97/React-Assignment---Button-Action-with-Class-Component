aae2c0a8b27e990cce18ff1306c8ef1d6014ea30
Aquib <aquibshaikh02397@gmail.com>
2020-11-10 19:46:04 +0530

    showMsg function added

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,27 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
-	};
+		this.state = {
+			click : ""
+		};
+		
+		this.showMsg = this.showMsg.bind(this);
+	}
+	showMsg(){
+		console.log(this.state.click);
+		this.setState({
+			click: this.state.click=="" ? 
+			(<p id='para'>
+				Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+				</p>) : ""
+	})}
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				<button onClick={this.showMsg}>Click Me!</button>
+				{this.state.click}
     		</div>
     	);
     }
