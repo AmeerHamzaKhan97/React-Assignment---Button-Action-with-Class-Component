e0bddc76e32f6f61ab6b8ff73119f6e644a6775c
AmeerHamzaKhan97 <hamzakn97@gmail.com>
2021-04-08 14:38:08 +0530

    react-class

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,34 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-			click : ""
-		};
-		
-		this.showMsg = this.showMsg.bind(this);
-	}
-	showMsg(){
-		console.log(this.state.click);
-		this.setState({
-			click: this.state.click=="" ? 
-			(<p id='para'>
-				Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
-				</p>) : ""
-	})}
+  constructor(props) {
+    super(props);
+    this.state = {
+      isParaShow: false,
+    };
+    this.showPara = this.showPara.bind(this);
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-				<button id="click" onClick={this.showMsg}>Click Me!</button>
-				{this.state.click}
-    		</div>
-    	);
-    }
+  showPara() {
+    console.log("run");
+    this.setState({ isParaShow: !this.state.isParaShow });
+  }
+
+  render() {
+    return (
+      <div id="main">
+        <button id="click" onClick={this.showPara}>
+          Show/hide paragarph
+        </button>
+        {this.state.isParaShow ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : null}
+      </div>
+    );
+  }
 }
-
-
 export default App;
-
